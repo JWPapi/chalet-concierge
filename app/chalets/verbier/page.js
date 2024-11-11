fcdf64732391cb6573a5ca77207264902db11d3f
JWPapi <juliantosun@gmail.com>
2024-11-11 17:13:06 +0100

    'deploy'

--- app/chalets/verbier/page.js
+++ app/chalets/verbier/page.js
@@ -14,30 +14,30 @@ export default function Verbier() {
     <main className="relative min-h-screen">
       <Hero 
         title="Verbier"
-        backgroundImage="https://www.skiinluxury.com/attachments/photos/verbier/hero-verbier.jpg"
+        backgroundImage="https://www.skiinluxury.com/attachments/photos/1170440/5ddd1e74-5490-48a7-8222-70931fc10e14.jpg"
       />
       <ConciergeSection location="Verbier" />
       <ImageGallery 
         location="Verbier"
         images={[
           {
-            src: "https://www.skiinluxury.com/attachments/photos/gallery/verbier-1.jpg",
+            src: "https://www.skiinluxury.com/attachments/photos/gallery/5df9f462-3058-4b61-b74c-14751fc10e14.jpg",
             alt: "Luxury Verbier Chalet Interior"
           },
           {
-            src: "https://www.skiinluxury.com/attachments/photos/gallery/verbier-2.jpg",
+            src: "https://www.skiinluxury.com/attachments/photos/chaletFeatureRD/5e380811-2988-462d-a2bf-377d1fc10e14.jpg",
             alt: "Verbier Chalet Exterior"
           },
           {
-            src: "https://www.skiinluxury.com/attachments/photos/gallery/verbier-3.jpg",
+            src: "https://www.skiinluxury.com/attachments/photos/chaletFeatureRD/607ed037-5bd0-44b5-b383-5ab60a000f09.jpg",
             alt: "Verbier Suite"
           },
           {
-            src: "https://www.skiinluxury.com/attachments/photos/gallery/verbier-4.jpg",
+            src: "https://www.skiinluxury.com/attachments/photos/chaletFeatureRD/5fdb914e-da70-46ce-92ea-6c970a000f09.jpg",
             alt: "Verbier Dining Area"
           },
           {
-            src: "https://www.skiinluxury.com/attachments/photos/gallery/verbier-5.jpg",
+            src: "https://www.skiinluxury.com/attachments/photos/1170440/5ddd1e74-5490-48a7-8222-70931fc10e14.jpg",
             alt: "Verbier Spa"
           }
         ]}
